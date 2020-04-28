fe56a2902428f932d4e987c542949ff67e646285
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-04-28 03:24:42 +0300

    React-Read Data From Array using JS

--- react-musicstore/src/components/CardContainer.js
+++ react-musicstore/src/components/CardContainer.js
@@ -3,10 +3,30 @@ import Card from './Card';
 
 export default class CardContainer extends Component {
     render() {
+        const cards = [
+            {
+            "id" : 1,
+            "img" : "img/strings.png",
+            "imgalt":"string",
+            "desc":"A very authentic and beautiful instrument!!",
+            "price" : 100.0,
+            "productname" : "Strings"
+           }, {
+            "id" : 2,
+            "img" : "img/redguitar.jpeg",
+            "imgalt":"redg",
+            "desc":"A really cool red guitar that can produce super cool music!!",
+            "price" : 299.0,
+            "productname" : "Red Guitar"
+          }
+        ];
         return (
             <div>
-                <Card key='1' img="img/strings.png" alt="strings" productName="Strings" price='100.0' desc="A very authentic and beautiful instrument!!" />
-                <Card key='2' img="img/redguitar.jpeg" alt="redg" productName="Red Guitar" price='299.0' desc="A really cool red guitar that can produce super cool music!!" />
+            {
+                cards.map(card =>
+                    <Card key={card.id} img={card.img} alt={card.imgalt} productName={card.productname} price={card.price} desc={card.desc} />
+                )
+            }
             </div>
         )
     }
