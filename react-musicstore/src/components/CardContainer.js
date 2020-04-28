6249b75482645fad53ee0dcec5efeef08fb7f734
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-04-28 04:16:43 +0300

    React-State

--- react-musicstore/src/components/CardContainer.js
+++ react-musicstore/src/components/CardContainer.js
@@ -2,26 +2,35 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 export default class CardContainer extends Component {
+    constructor(props) {
+        //pass props to the parent component
+        super(props);
+        //initialize the state object for this component
+        this.state = {
+            cards: [
+                {
+                    "id": 1,
+                    "img": "img/strings.png",
+                    "imgalt": "string",
+                    "desc": "A very authentic and beautiful instrument!!",
+                    "price": 100.0,
+                    "productname": "Strings"
+                }, 
+                {
+                    "id": 2,
+                    "img": "img/redguitar.jpeg",
+                    "imgalt": "redg",
+                    "desc": "A really cool red guitar that can produce super cool music!!",
+                    "price": 299.0,
+                    "productname": "Red Guitar"
+                }
+            ]
+        };
+    }
+
     render() {
-        const cards = [
-            {
-                "id": 1,
-                "img": "img/strings.png",
-                "imgalt": "string",
-                "desc": "A very authentic and beautiful instrument!!",
-                "price": 100.0,
-                "productname": "Strings"
-            }, {
-                "id": 2,
-                "img": "img/redguitar.jpeg",
-                "imgalt": "redg",
-                "desc": "A really cool red guitar that can produce super cool music!!",
-                "price": 299.0,
-                "productname": "Red Guitar"
-            }
-        ];
         //get a list of JSX elements representing each card
-        const cardItems = cards.map(
+        const cardItems = this.state.cards.map(
             card => <Card key={card.id} {...card} />
         );
         return (
