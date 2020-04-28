1277b94c9d7ca6a61e87fcbd8262f8b0447c5161
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-04-28 03:25:33 +0300

    React-Read Data From Array using JSX

--- react-musicstore/src/components/CardContainer.js
+++ react-musicstore/src/components/CardContainer.js
@@ -5,29 +5,29 @@ export default class CardContainer extends Component {
     render() {
         const cards = [
             {
-            "id" : 1,
-            "img" : "img/strings.png",
-            "imgalt":"string",
-            "desc":"A very authentic and beautiful instrument!!",
-            "price" : 100.0,
-            "productname" : "Strings"
-           }, {
-            "id" : 2,
-            "img" : "img/redguitar.jpeg",
-            "imgalt":"redg",
-            "desc":"A really cool red guitar that can produce super cool music!!",
-            "price" : 299.0,
-            "productname" : "Red Guitar"
-          }
+                "id": 1,
+                "img": "img/strings.png",
+                "imgalt": "string",
+                "desc": "A very authentic and beautiful instrument!!",
+                "price": 100.0,
+                "productname": "Strings"
+            }, {
+                "id": 2,
+                "img": "img/redguitar.jpeg",
+                "imgalt": "redg",
+                "desc": "A really cool red guitar that can produce super cool music!!",
+                "price": 299.0,
+                "productname": "Red Guitar"
+            }
         ];
+        //get a list of JSX elements representing each card
+        const cardItems = cards.map(
+            card => <Card key={card.id} img={card.img} alt={card.imgalt} productName={card.productname} price={card.price} desc={card.desc} />
+        );
         return (
             <div>
-            {
-                cards.map(card =>
-                    <Card key={card.id} img={card.img} alt={card.imgalt} productName={card.productname} price={card.price} desc={card.desc} />
-                )
-            }
+                {cardItems}
             </div>
-        )
+        );
     }
 }
