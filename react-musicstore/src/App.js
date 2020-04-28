816dd440158f69e2579e82f5e692184e71411458
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-04-28 03:06:53 +0300

    React-Create the Card Component

--- react-musicstore/src/App.js
+++ react-musicstore/src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className="btn btn-success">Click me Here</button>
-      </header>
+    <Card />
       
     </div>
   );
---
+++ react-musicstore/src/components/Card.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+    render() {
+        const img = "img/strings.png";
+        const imgalt = "string";
+        const desc = "A very authentic and beautiful instrument!!";
+        const price = 100;
+        const productName = "Strings";
+        return (
+            <div className="col-md-6 col-lg-4 d-flex align-items-stretch">
+            <div className="card mb-3">
+                <img className="card-img-top" src={img} alt={imgalt} />
+                <div className="card-body">
+                    <h4 className="card-title">{productName}</h4>
+                    Price: <strong>{price}</strong>
+                    <p className="card-text">{desc}</p>
+                    <a href="#" className="btn btn-primary">Buy</a>
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
